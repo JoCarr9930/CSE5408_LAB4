265521e600878f8167bce2fd2aabc085a5aedfd3
BrianSaldana <brianxsaldana@gmail.com>
2022-02-23 10:26:01 -0800

    Empty

--- Lab4_Solutions.py
+++ Lab4_Solutions.py
@@ -6,14 +6,3 @@
 # CSE 5408
 # Spring 2022
 # Lab 4
-
-#Brian Saldana
-#CSE 5408 
-#Lab 4
-#Problem 1: Write a program containing a function to reverse a user inputted string.
-
-def Reverse(x):
-    return x[::-1]
-
-x = input("Enter a String !\n")
-print("This is your String Reversed!\n"+ Reverse(x))
